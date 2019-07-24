f8b46ea5c396afcf67d857cb74147c672e4a9c64
Ben <bigriffin94@outlook.com>
2019-07-24 15:00:23 -0700

    300PM commit

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,7 +15,16 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
-
+//Constructor Function
+function GameObject(attributes) {
+  this.createdAt = attributes.createdAt;
+  this.name = attributes.name;
+  this.dimensions = attributes.dimensions;
+};
+//Prototype 
+GameObject.prototype.destroy = function () {
+  return `${this.name} was removed from the game. `;
+}
 /*
   === CharacterStats ===
   * healthPoints
@@ -23,15 +32,43 @@
   * should inherit destroy() from GameObject's prototype
 */
 
+function CharacterStats(attributes){
+  GameObject.call(this, attributes)
+  this.healthPoints = attributes.healthPoints;
+};
+//Object Create 
+CharacterStats.prototype = Object.create(GameObject.prototype);
+//Prototype 
+CharacterStats.prototype.takeDamage = function(){
+  return ` ${this.name} took damage `;
+};
+
+
 /*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
+                     === Humanoid (Having an appearance or character resembling that of a human.) ===
   * team
   * weapons
   * language
   * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+  * 
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+//needs a call
+function Humanoid(attributes) {
+  CharacterStats.call(this, attributes)
+  this.team = attributes.team;
+  this.weapons = attributes.weapons;
+  this.language = attributes.language;
+};
+ //Object.create
+ Humanoid.prototype = Object.create(CharacterStats.prototype);
+//Prototype
+Humanoid.prototype.greet = function(){
+  return `${this.name} offers a greeting in ${this.language}.`;
+};
+
+
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -41,7 +78,6 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +138,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Eplicit - we can choose what 'this' keyword is using --> .call(), .apply(), OR bin(), ...
+* 2. Implicit - the JS system *automatically* assigns 'this' to the left of the '.'
+* 3. Window - where the entire JS library is hsoted,
+* 4. New - constructs a NEW object;
 *
 * write out a code example of each explanation above
 */
